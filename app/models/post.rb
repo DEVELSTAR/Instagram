90cac3e6dc9a5a8edab73f729be8fdcc3fdc8b6c
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-09-21 06:13:30 +0530

    hashtag and search feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,21 @@
 class Post < ApplicationRecord
+
+  after_commit :create_hash_tags, on: :create
+    
+  def create_hash_tags
+    extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+
+  def extract_name_hash_tags
+    description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+  end
+
   belongs_to :user
   has_one_attached :image
-
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
 
 
   validate :image_presence
@@ -9,5 +23,8 @@ class Post < ApplicationRecord
   def image_presence
     errors.add(:image, "can't be blank") unless image.attached?
   end
+
+
+
 
 end
